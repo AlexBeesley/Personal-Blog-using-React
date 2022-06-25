0b702a0930d32089ff7bc8addcb19282b339e8a7
David Beesley <D.Beesley1@uni.brighton.ac.uk>
2022-06-25 16:59:41 +0100

    error page css overhaul

--- src/Pages/ErrorPage.tsx
+++ src/Pages/ErrorPage.tsx
@@ -5,6 +5,7 @@ function ErrorPage() {
   return (
     <div className={Styles.error}>
       <p><i className="fa-solid fa-bug"></i> 404: Page not found</p>
+      <div className={Styles.spacer}></div>
     </div>
   );
 }
