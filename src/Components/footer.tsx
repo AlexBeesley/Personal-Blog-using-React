45d7696fe98627f2e9473f0edfee5f365f78eaa3
David Beesley <David.Beesley@tpr.gov.uk>
2022-06-27 13:37:21 +0100

    github username change

--- src/Components/footer.tsx
+++ src/Components/footer.tsx
@@ -11,7 +11,7 @@ export default function Footer () {
                 title="LinkedIn"><i className="fa-brands fa-linkedin"></i></a>
                 <a href="https://www.instagram.com/brighton_by_the_sea/" target="_blank" rel="noreferrer"
                 title="Instagram"><i className="fa-brands fa-instagram"></i></a>
-                <a href="https://github.com/DABeesleyTPR" target="_blank" rel="noreferrer"
+                <a href="https://github.com/AlexBeesley" target="_blank" rel="noreferrer"
                 title="GitHub"><i className="fa-brands fa-github"></i></a>
             </div>
         </footer>
