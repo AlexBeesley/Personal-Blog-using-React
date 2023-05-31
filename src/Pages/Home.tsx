da21c573b6f46b67d77a190bfe1964db536a289c
Alex <david.beesley1234@gmail.com>
2023-05-31 17:55:34 +0100

    content update

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -152,136 +152,13 @@ function Home() {
         <div className={Styles.col2}>
           <Card title="About me">
             <p>
-              I am a 22-year-old .NET Developer Intern for{' '}
-              <a
-                className={Styles.inlineLink}
-                href="https://www.thepensionsregulator.gov.uk/"
-                target="_blank"
-                title="TPR"
-              >
-                The Pensions Regulator
-              </a>
-              . I am a self-taught programmer, with a passion for new and
-              emerging technologies. My interest in computing stems from the age
-              of 12, when my parents brought me one of the very first editions
-              of{' '}
-              <a
-                className={Styles.inlineLink}
-                href="https://www.raspberrypi.org/"
-                target="_blank"
-                title="What is a Raspberry Pi?"
-              >
-                Raspberry Pi
-              </a>{' '}
-              Model B which came out in February 2012, which came in a box
-              signed by The Raspberry Pi Foundation’s CEO and founder, David
-              Breben. On this early edition there was little support at the time
-              and even getting it to boot off an SD Card preloaded with Raspbian
-              (a flavour of{' '}
-              <a
-                className={Styles.inlineLink}
-                href="https://en.wikipedia.org/wiki/List_of_Linux_distributions"
-                target="_blank"
-                title="List of Linux distributions on Wikipedia"
-              >
-                Linux
-              </a>{' '}
-              for the Pi based on the Debian distro) and then learning the
-              basics of Linux to get the most out of my Pi was a challenge for a
-              12-year-old, but one that I enjoyed deeply and which inspired the
-              direction of my academic career in the following years.
+              I am a 22-year-old .NET Developer Intern at <a className={Styles.inlineLink} href="https://www.thepensionsregulator.gov.uk/" target="_blank" title="TPR">The Pensions Regulator</a>. As a self-taught programmer, I have a passion for new and emerging technologies. My interest in computing began at the age of 12 when my parents gifted me one of the very first editions of the <a className={Styles.inlineLink} href="https://www.raspberrypi.org/" target="_blank" title="What is a Raspberry Pi?">Raspberry Pi</a>, which came out in February 2012. The Raspberry Pi was signed by the Raspberry Pi Foundation's CEO and founder, David Breben. At that time, there was limited support for the device, and even getting it to boot off an SD Card preloaded with Raspbian, a Linux flavor for the Pi based on the Debian distro, was a challenge for a 12-year-old. However, I embraced the challenge and enjoyed delving into the basics of Linux to make the most out of my Pi. This experience deeply inspired the direction of my academic career in the following years.
             </p>
             <p>
-              Fast-forward to 2016, I had taught myself basic Python and had
-              touched on HTML, CSS and{' '}
-              <a
-                className={Styles.inlineLink}
-                href="https://www.w3schools.com/whatis/"
-                target="_blank"
-                title="Web Development learning roadmaps"
-              >
-                basic web development
-              </a>
-              . Achieving an A grade in GCSE Computer Science, I wanted to focus
-              fully on my passion and took a BTEC in IT, the following year,
-              where I developed an interest in Artificial Intelligence and
-              Machine Learning. Inspired by this cutting-edge technology with
-              the seemingly infinite potential it made sense to me to pursue{' '}
-              <a
-                className={Styles.inlineLink}
-                href="https://www.brighton.ac.uk/courses/study/computer-science-with-artificial-intelligence-bsc-hons.aspx"
-                target="_blank"
-                title="Course overview"
-              >
-                Computer Science with AI at Brighton University
-              </a>
-              . During my 2 years of University, I have deepened my
-              understanding of computing principles such as Binary Logic, Data
-              Representation, Networking, the Software Development Cycle and
-              Cybersecurity while learning technologies like Java,{' '}
-              <a
-                className={Styles.inlineLink}
-                href="https://www.tensorflow.org/"
-                target="_blank"
-                title="What is TensorFlow?"
-              >
-                Tensorflow
-              </a>{' '}
-              and SQL.
+              In 2016, I taught myself basic Python and dabbled in HTML, CSS, and basic web development. I achieved an A grade in GCSE Computer Science and decided to fully focus on my passion. The following year, I pursued a BTEC in IT, during which I developed a strong interest in Artificial Intelligence and Machine Learning. The seemingly infinite potential of these cutting-edge technologies captivated me, and I decided to pursue a degree in <a className={Styles.inlineLink} href="https://www.brighton.ac.uk/courses/study/computer-science-with-artificial-intelligence-bsc-hons.aspx" target="_blank" title="Course overview">Computer Science with AI at Brighton University</a>. Over the course of my two years at university, I deepened my understanding of fundamental computing principles such as Binary Logic, Data Representation, Networking, the Software Development Cycle, and Cybersecurity. I also gained hands-on experience with technologies like Java, <a className={Styles.inlineLink} href="https://www.tensorflow.org/" target="_blank" title="What is TensorFlow?">TensorFlow</a>, and SQL.
             </p>
             <p>
-              Before returning for my final year, I choose to do a year
-              placement with The Pensions Regulator, as a .NET Developer and{' '}
-              <a
-                className={Styles.inlineLink}
-                href="https://about.gitlab.com/topics/devops/"
-                target="_blank"
-                title="What is DevOps?"
-              >
-                DevOps
-              </a>{' '}
-              Specialist. During this placement, I have learnt and developed in
-              widely used industry technologies such as .NET APIs and MVCs, C#
-              unit and integration testing, JavaScript/TypeScript, React and
-              have familiarised myself with both{' '}
-              <a
-                className={Styles.inlineLink}
-                href="https://www.sitecore.com/"
-                target="_blank"
-                title="What is Sitecore?"
-              >
-                Sitecore
-              </a>{' '}
-              and{' '}
-              <a
-                className={Styles.inlineLink}
-                href="https://umbraco.com/"
-                target="_blank"
-                title="What is Umbraco?"
-              >
-                Umbraco
-              </a>
-              . While following an Agile workflow within my team, using Azure
-              DevOps to track work items using Kanban, covering Git extensively
-              with peer-reviewed Pull Requests, touching on{' '}
-              <a
-                className={Styles.inlineLink}
-                href="https://azure.microsoft.com/en-gb/"
-                target="_blank"
-                title="What is Azure?"
-              >
-                Azure
-              </a>{' '}
-              Pipelines, CI/CD Deployments and{' '}
-              <a
-                className={Styles.inlineLink}
-                href="https://www.docker.com/"
-                target="_blank"
-                title="What is Docker?"
-              >
-                Docker
-              </a>{' '}
-              containers.
+              Before returning for my final year, I embarked on a year-long placement with The Pensions Regulator as a .NET Developer and <a className={Styles.inlineLink} href="https://about.gitlab.com/topics/devops/" target="_blank" title="What is DevOps?">DevOps</a> Specialist. During this placement, I have honed my skills and gained proficiency in widely-used industry technologies such as .NET APIs and MVCs, C# unit and integration testing, JavaScript/TypeScript, and React. I have also familiarised myself with both <a className={Styles.inlineLink} href="https://www.sitecore.com/" target="_blank" title="What is Sitecore?">Sitecore</a> and <a className={Styles.inlineLink} href="https://umbraco.com/" target="_blank" title="What is Umbraco?">Umbraco</a>. While following an Agile workflow within my team, using Azure DevOps to track work items using Kanban, covering Git extensively with peer-reviewed Pull Requests, touching on <a className={Styles.inlineLink} href="https://azure.microsoft.com/en-gb/" target="_blank" title="What is Azure?">Azure</a> Pipelines, CI/CD Deployments, and <a className={Styles.inlineLink} href="https://www.docker.com/" target="_blank" title="What is Docker?">Docker</a> containers. I look forward to applying my skills and knowledge gained from my academic journey and industry experience to contribute to the growth and success of The Pensions Regulator as I continue to develop as a professional .NET Developer.
             </p>
           </Card>
         </div>
