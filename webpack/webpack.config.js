12b98d6453417dbe7166902d5b31f3a7ae55920d
Alex <david.beesley1234@gmail.com>
2023-12-25 21:05:19 +0000

    fixed deploy issue

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,18 +1,9 @@
-const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.common.js');
+const { merge } = require('webpack-merge')
+const commonConfig = require('./webpack.common.js')
 
 module.exports = (envVars) => {
-  const { env } = envVars;
-  const envConfig = require(`./webpack.${env}.js`);
-  const config = merge(commonConfig, envConfig);
-
-  config.module.rules.push({
-    test: /\.tsx?$/,
-    exclude: /node_modules/,
-    use: 'ts-loader',
-  });
-
-  config.resolve.extensions.push('.ts', '.tsx');
-
-  return config;
-};
+  const { env } = envVars
+  const envConfig = require(`./webpack.${env}.js`)
+  const config = merge(commonConfig, envConfig)
+  return config
+}
